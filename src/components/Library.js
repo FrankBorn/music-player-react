618e59bf9f653da4ab1bd53030b74b2f67bcbad0
FrankBorn <frankborn2021code@outlook.com>
2022-05-29 16:52:51 +0200

    added styling on active song

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,7 +1,7 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Libary = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
+const Libary = ({ songs, setCurrentSong, audioRef, isPlaying, setSongs }) => {
   return (
     <div className="library">
       <h2>Library</h2>
@@ -15,6 +15,7 @@ const Libary = ({ songs, setCurrentSong, audioRef, isPlaying }) => {
             key={song.id}
             audioRef={audioRef}
             isPlaying={isPlaying}
+            setSongs={setSongs}
           />
         ))}
       </div>
